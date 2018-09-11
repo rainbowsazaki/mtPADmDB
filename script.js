4161141ab26b233ea3d942157235db2c97fa6c93
rainbowsazaki <rainbowsazaki@gmail.com>
2018-09-11 15:11:24 +0900

    [add] モンスターアイコン表示コンポーネントに、アイコン画像がない場合にモンスター番号を表示する機能を追加。

--- script.js
+++ script.js
@@ -674,6 +674,7 @@ Vue.component('monsterIcon', {
   <div v-else style="display: inline-block; background-color: #ccc; position:relative; vertical-align:bottom; border: 1px solid #bbb; border-bottom-width: 2px; border-radius: 6%;" :style="{ width: width, height: height }"> 
     <img v-if="hasAttr0" style="position:absolute; left:  2%; top:    2%; width: 23%; height: 23%;" :src="attrPath0" />
     <img v-if="hasAttr1" style="position:absolute; right: 2%; bottom: 2%; width: 23%; height: 23%;" :src="attrPath1" />
+    <div :style="{ fontSize: fontSize, lineHeight: height }" style="text-align: center; overflow: hidden; color: #aaa;">{{no}}</div>
   </div>
   `,
   props: [ 'no', 'monsterTable', 'imageTable', 'width', 'height' ],
@@ -688,6 +689,11 @@ Vue.component('monsterIcon', {
     iconPath: function () { return `./monsterIconsLog/icon_${this.no}_${this.imageTable[this.no].id}.jpg`; },
     attrPath0: function () { return `./image/attribute/${this.attributes[0]}.png`; },
     attrPath1: function () { return `./image/attribute/${this.attributes[1]}.png`; },
+    
+    fontSize: function () {
+      var ret = /^([\d|.]*)(.*)$/.exec(this.width);
+      return ret[1] / 3 + ret[2];
+    }
   }
 })
 
