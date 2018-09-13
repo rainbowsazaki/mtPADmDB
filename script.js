810367b5f10f2da0dca1de54e69bef3688933f9f
rainbowsazaki <rainbowsazaki@gmail.com>
2018-09-13 22:35:09 +0900

    [add] モンスター情報の直近50件の編集履歴を確認できるページを追加。
    直近50件のモンスター情報編集履歴の、日時、モンスター番号・名前、編集コメントを一覧表示する。
    メニューからのリンクは用意しない、管理用隠し機能的な位置づけ。

--- script.js
+++ script.js
@@ -1016,6 +1016,61 @@ var componentMonsterData = {
 
 
 /**
+ * モンスター情報変更履歴ページコンポーネント
+ */
+var componentHistory = {
+  name: 'pageHistory',
+  template: '#templateHistory',
+  pageTitle: function () {
+    var str = `No.${this.$route.params.no || this.monsterData.no} ${this.monsterData.name}`;
+    if (this.isHistory) { str += ` (${this.monsterData.datetime})`}
+    return '履歴一覧';
+  },
+  props: [ 'no' ],
+  data: function () {
+    return {
+      /** 履歴情報 */
+      histories: null,
+    };
+  },
+  created: function () {
+    this.loadHistories();
+  },
+  
+  methods: {
+    /** 履歴リストを取得する。 */
+    loadHistories: function () {
+      this.isLoadingHistory = true;
+      axios.get('./api.cgi', {
+        params: { mode: 'monsterHistory' }
+      })
+      .then(response => {
+        this.histories = response.data;
+      });
+    },
+
+    /** 指定された履歴情報が今有効なデータかどうかを取得する。 */
+    isActiveHistory: function (history) {
+      return history.state == 1;
+    },
+    /** 指定された番号のモンスターの名前を取得する。 */
+    monsterName: function (no) {
+      return (this.monsterTable[no] || { name: '' }).name;
+    }
+  },
+
+  computed: {
+    monsterTable: function () { return this.$store.state.monsterTable; },
+
+    /** 編集履歴の表示かどうか。 */
+    isHistory: function () {
+      return (this.$route.name == 'history');
+    },
+  },
+};
+
+
+/**
  * モンスター情報編集ページコンポーネント
  */
 var componentMonsterEdit = {
@@ -1563,6 +1618,11 @@ var router = new VueRouter({
     {
       path: '/about',
       component: componentAbout,
+    },
+    {
+      path: '/history',
+      component: componentHistory,
+      props: true
     },
 
     {
