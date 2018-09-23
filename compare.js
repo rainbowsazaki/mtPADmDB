431e1abdef4738d6f1c7e8ca168c17dd52a11e03
rainbowsazaki <rainbowsazaki@gmail.com>
2018-09-23 21:13:33 +0900

    [update] パラメータ比較処理の表示内容にタイプ、属性、覚醒、超覚醒、潜在キラー、限界突破時パラメータを追加。
    超覚醒、限界突破時パラメータは比較対象のうち少なくとも1体が限界突破に対応している場合に表示するように。

--- compare.js
+++ compare.js
@@ -30,6 +30,56 @@ var componentCompare = {
       <td v-for="data in monsterDatas">{{data.name}}</td>
     </tr>
     <tr class="thead-light">
+      <th>タイプ</th>
+      <td v-for="data in monsterDatas">
+        <span v-if="data.types[0] == null">不明</span>
+        <span v-else v-for="type in data.types" v-if="type !== 0">
+          <img v-if="type !== null" :src="\`./image/type/\${type}.png\`" alt="" style="width:1.5em; height: 1.5em;">
+        </span>
+      </td>
+    </tr>
+    <tr class="thead-light">
+      <th>属性</th>
+      <td v-for="data in monsterDatas">
+        <span v-if="data.attributes[0] === null">不明</span>
+        <img  v-for="attr in data.attributes" v-if="attr != 0 && attr !== null" style="width: 1.5em; height: 1.5em;" :src="\`./image/attribute/\${attr}.png\`">
+      </td>
+    </tr>
+
+    <tr class="thead-light">
+      <th>覚醒</th>
+      <td v-for="data in monsterDatas">
+      <span v-if="data.awakens[0] === 0">なし</span>
+      <span v-else-if="data.awakens[0] === null">不明</span>
+      <ul v-else style="list-style: none; margin: 0px; padding: 0px;">
+        <li v-for="awaken in data.awakens">
+          <img v-if="awaken !== 0" :src="'./image/awaken/' + awaken + '.png'" style="width: 1.5em; height: 1.5em;" :title="awakenTable[awaken].name + '\\n\\n' + awakenTable[awaken].description" />
+        </li>
+      </ul>
+    </td>
+    </tr>
+    <tr class="thead-light" v-if="hasOverLimit">
+      <th>超覚醒</th>
+      <td v-for="data in monsterDatas">
+        <span v-for="superAwaken in data.superAwakens">
+          <img v-if="superAwaken !== null" :src="'./image/awaken/' + superAwaken + '.png'" style="width: 1.5em; height: 1.5em;" :title="awakenTable[superAwaken].name + '\\n\\n' + awakenTable[superAwaken].description" />
+        </span>
+      </td>
+    </tr>
+    <tr class="thead-light">
+      <th>潜在キラー</th>
+      <td v-for="data in monsterDatas">
+        <span v-if="data.types[0] === null">不明</span>
+        <ul v-else-if="getSenzaiKillerNos(data).length" style="list-style: none; margin: 0px; padding: 0px;">
+          <li v-for="senzaiKillerType in getSenzaiKillerNos(data)" style="display: inline-block">
+            <img :src="\`./image/senzaiKiller/\${senzaiKillerType}.png\`" :alt="\`\${typeTable[senzaiKillerType].name}キラー\`"style="width: auto; height: 1.5em;" />
+          </li>
+        </ul>
+        <span v-else>なし</span>
+      </td>
+    </tr>
+    
+    <tr class="thead-light">
       <th>HP</th>
       <td v-for="data in monsterDatas" class="text-right">{{data.maxParam.hp}}</td>
     </tr>
@@ -41,6 +91,23 @@ var componentCompare = {
       <th>回復</th>
       <td v-for="data in monsterDatas" class="text-right">{{data.maxParam.recovery}}</td>
     </tr>
+    <template v-if="hasOverLimit">
+      <tr class="thead-light">
+        <th :colspan="monsterDatas.length + 1">限界突破時</th>
+      </tr>
+      <tr class="thead-light">
+        <th>HP</th>
+        <td v-for="data in monsterDatas" class="text-right">{{data.overLimitParam.hp}}</td>
+      </tr>
+      <tr class="thead-light">
+        <th>攻撃</th>
+        <td v-for="data in monsterDatas" class="text-right">{{data.overLimitParam.attack}}</td>
+      </tr>
+      <tr class="thead-light">
+        <th>回復</th>
+        <td v-for="data in monsterDatas" class="text-right">{{data.overLimitParam.recovery}}</td>
+      </tr>
+    </template>
   </table>
   </template>
 </div>`,
@@ -60,6 +127,9 @@ var componentCompare = {
   computed: {
     monsterTable () { return this.$store.state.monsterTable; },
     imageTable() { return this.$store.state.imageTable; },
+    booleanTable() { return constData.booleanTable; },
+    typeTable () { return constData.typeTable; },
+    awakenTable () { return constData.awakenTable; },
 
     /** 比較するモンスター情報の配列を取得する。 */
     monsterDatas: function () {
@@ -69,6 +139,10 @@ var componentCompare = {
     isEnableMonsterDatas: function () {
       return (this.monsterDatas.indexOf(null) == -1);
     },
+    /** 限界突破可能なモンスターがいるかどうかを取得する。 */
+    hasOverLimit: function () {
+      return this.monsterDatas.find((o) => o.overLimit == 1);
+    }
   },
 
   methods: {
@@ -106,7 +180,20 @@ var componentCompare = {
           this.$store.commit('setErrors', [ errorMessage ]);
         }
       );
-    }
+    },
+
+    getSenzaiKillerNos: function (monsterData) {
+      // 合成できないものは潜在覚醒を降ることができないので無し。
+      if (!this.canAddPlus) { return [] }
+      var killerNoSet = new Set();
+      for (var type of monsterData.types) {
+        for (var killerNo of this.typeTable[type].senzaiKiller) {
+          killerNoSet.add(killerNo);
+        }
+      }
+      return Array.from(killerNoSet).sort(((a, b) => a - b ));
+
+    },
   }
 };
 
