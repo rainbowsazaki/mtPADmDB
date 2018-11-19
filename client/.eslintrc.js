8b59b81b273d11bc0ac0ff62d9a8b48e5c8833c0
rainbowsazaki <rainbowsazaki@gmail.com>
2018-11-19 17:35:41 +0900

    [add] client フォルダに eslint の設定ファイルをコピー。
    上位のフォルダで使用しているものをそのままコピー。

---
+++ client/.eslintrc.js
@@ -0,0 +1,49 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "jquery": true
+    },
+    "globals": {
+        "Vue": true,
+        "VueRouter": true,
+        "Vuex": true,
+        "axios": true,
+        "gtag": true,
+        "twttr": true
+    },
+    "extends": ['vue', 'plugin:vue/recommended'],
+    "parserOptions": {
+        "ecmaVersion": 2016
+    },
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-var": "error",
+        "no-irregular-whitespace": "error",
+        "block-spacing": "error",
+        "comma-spacing": "error",
+        "func-call-spacing": "error",
+        "keyword-spacing": "error",
+        "no-trailing-spaces": [
+            "error",
+            {"skipBlankLines": true }
+        ],
+        "no-whitespace-before-property": "error",
+        "arrow-spacing": "error"
+    }
+};
