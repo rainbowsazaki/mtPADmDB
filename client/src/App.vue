e6c7cd00695e1e1c8ff4dce41bd916e7fbb0a780
rainbowsazaki <rainbowsazaki@gmail.com>
2018-11-20 18:51:54 +0900

    [clean] App.vue の Vue テンプレートの展開括弧の内側のスペースや空要素の終了タグ周りを変更。
    前２つのコミットでの変更忘れ。

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,12 +3,12 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top" style="box-shadow: 0px 0px 4px rgba(0,0,0,0.6);">
       <router-link class="navbar-brand" to="/">mtPADmDB</router-link>
       <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="ナビゲーションの切替">
-        <span class="navbar-toggler-icon"></span>
+        <span class="navbar-toggler-icon" />
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           <li v-for="(navi, n) in navis" class="nav-item" @click="hideNavi" :key="n">
-            <router-link class="nav-link" :class="{ active: $route.path === navi.to }" :to="navi.to">{{navi.text}}</router-link>
+            <router-link class="nav-link" :class="{ active: $route.path === navi.to }" :to="navi.to">{{ navi.text }}</router-link>
           </li>
         </ul>
       </div>
@@ -18,7 +18,7 @@
       <div style="position:fixed; z-index: 2;">
         <transition-group name="error">
           <div v-for="error in errors" :key="error" class="alert alert-warning alert-dismissible fade show" role="alert">
-            <strong>注意!</strong> {{error}}
+            <strong>注意!</strong> {{ error }}
             <button type="button" @click="$store.commit('deleteError', error)" class="close" aria-label="閉じる">
               <span aria-hidden="true">&times;</span>
             </button>
@@ -26,7 +26,7 @@
         </transition-group>
         <transition-group name="message">
           <div v-for="message in messages" :key="message" class="alert alert-success alert-dismissible fade show" role="alert">
-            {{message}}
+            {{ message }}
           </div>
         </transition-group>
       </div>
@@ -34,17 +34,17 @@
       <nav aria-label="パンくずリスト">
         <ol class="breadcrumb">
           <li v-for="breadcrumb in $root.breadcrumbs" :aria-current="breadcrumb.link ? '' : 'page '" class="breadcrumb-item" :class="{ active: !breadcrumb.link }" :key="breadcrumb.link">
-            <router-link v-if="breadcrumb.link" :to="breadcrumb.link">{{breadcrumb.text}}</router-link>
-            <template v-else>{{breadcrumb.text}}</template>
+            <router-link v-if="breadcrumb.link" :to="breadcrumb.link">{{ breadcrumb.text }}</router-link>
+            <template v-else>{{ breadcrumb.text }}</template>
           </li>
         </ol>
       </nav>
-      <router-view v-if="$store.state.monsterTable"></router-view>
+      <router-view v-if="$store.state.monsterTable" />
       <div style="margin-top:2rem; display: flex; justify-content: space-between; flex-wrap: wrap;">
-        <iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=myfavoriteday-22&language=ja_JP&o=9&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B01CDP1DMS&linkId=2fde5a6504e57a723c17da60307b4591"></iframe>
-        <iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=myfavoriteday-22&language=ja_JP&o=9&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=4047333301&linkId=969a4d654c0891806ec194ae22c2d35d"></iframe>
-        <iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=myfavoriteday-22&language=ja_JP&o=9&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B0719QSRBV&linkId=22637bfb9546ec8a94a8a1e112a30b16"></iframe>
-        <iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=myfavoriteday-22&language=ja_JP&o=9&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B07FF4FPPV&linkId=c6edf491b7d7333cf8ffebd9a0d9b039"></iframe>
+        <iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=myfavoriteday-22&language=ja_JP&o=9&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B01CDP1DMS&linkId=2fde5a6504e57a723c17da60307b4591" />
+        <iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=myfavoriteday-22&language=ja_JP&o=9&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=4047333301&linkId=969a4d654c0891806ec194ae22c2d35d" />
+        <iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=myfavoriteday-22&language=ja_JP&o=9&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B0719QSRBV&linkId=22637bfb9546ec8a94a8a1e112a30b16" />
+        <iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=myfavoriteday-22&language=ja_JP&o=9&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B07FF4FPPV&linkId=c6edf491b7d7333cf8ffebd9a0d9b039" />
       </div>
     </div>
   </div>
