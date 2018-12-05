3cfb040047185063fcd7920857e0876ec7b3bd42
rainbowsazaki <rainbowsazaki@gmail.com>
2018-12-05 21:31:15 +0900

    [update] モンスターランキングページで、複数の条件を登録しておいて切り替えて表示できるよう変更。
    切替方法は、仮実装として select 要素。

--- client/src/views/pageRanking.vue
+++ client/src/views/pageRanking.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
+    <select v-model.number="rankingSettingIndex">
+      <option v-for="(setting, n) in $options.rankingSettings" :value="n" :key="`rankingSetting${n}`">{{ setting.title }}ランキング</option>
+    </select>
     <h2>{{ rankingSetting.title }}ランキング</h2>
     <p v-if="rankingSetting.description">{{ rankingSetting.description }}</p>
     <p>※このサイトに登録されているモンスターでのランキングです。</p>
@@ -33,24 +36,42 @@ export default {
   pageTitle: function () {
     return this.rankingSetting.title + 'ランキング';
   },
+  /** ランキング設定の配列。 */
+  rankingSettings: [
+    {
+      title: 'HP',
+      description: 'モンスターのレベル最大時のHPのランキングです。',
+      columns: [
+        { name: 'HP', func: data => data.maxParam.hp },
+        { name: '攻撃', func: data => data.maxParam.attack },
+        { name: '回復', func: data => data.maxParam.recovery }
+      ],
+      sortColumn: 0
+    },
+    {
+      title: '攻撃',
+      description: 'モンスターのレベル最大時の攻撃のランキングです。',
+      columns: [
+        { name: 'HP', func: data => data.maxParam.hp },
+        { name: '攻撃', func: data => data.maxParam.attack },
+        { name: '回復', func: data => data.maxParam.recovery }
+      ],
+      sortColumn: 1
+    }
+  ],
   data: function () {
     return {
-      rankingSetting: {
-        title: '最大レベル時HP',
-        description: 'モンスターのレベル最大時のHPのランキングです。',
-        columns: [
-          { name: 'HP', func: data => data.maxParam.hp },
-          { name: '攻撃', func: data => data.maxParam.attack },
-          { name: '回復', func: data => data.maxParam.recovery }
-        ],
-        sortColumn: 0
-      }
+      /** 現在使用するランキング設定のインデックス。 */
+      rankingSettingIndex: 0
     };
   },
   computed: {
     monsterTable () { return this.$store.state.monsterTable; },
     imageTable () { return this.$store.state.imageTable; },
 
+    /** 現在使用するランキング設定。 */
+    rankingSetting () { return this.$options.rankingSettings[this.rankingSettingIndex]; },
+    /** 現在の設定でのランキング結果を格納した配列。 */
     rankInfos () {
       const rankInfos = [];
       for (const key in this.monsterTable) {
@@ -61,6 +82,9 @@ export default {
       rankInfos.sort((a, b) => b.columns[sortColumn] - a.columns[sortColumn]);
       return rankInfos;
     }
+  },
+  watch: {
+    rankingSetting: '$_mixinForPage_updateTitle'
   }
 };
 
