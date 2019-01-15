2374876dfbbd698c2550b19c118308ce18ff86f8
rainbowsazaki <rainbowsazaki@gmail.com>
2019-01-15 21:20:42 +0900

    [update] モンスター絞り込み選択コンポーネント内の覚醒選択処理を、コンポーネントを使用する形に変更。

--- client/src/components/monsterFilterSetting.vue
+++ client/src/components/monsterFilterSetting.vue
@@ -78,19 +78,7 @@
         <div class="form-group row">
           <label class="col-sm-2 col-form-label">覚醒</label>
           <div class="col-sm-10">
-            <div style="display: inline-block; margin-bottom: 24px; padding: 4px; border: solid #CCC 1px;">
-              <span v-for="i in 9" :key="`selectedAwaken_${i}`">
-                <img style="width: 24px; height: 24px; background: #DDD; border-radius: 4px; margin-right: 6px;" :class="{ cursor: filter.awaken[i - 1] ? 'pointer' : undefined }" :src="filter.awaken[i - 1] ? `./image/awaken/${filter.awaken[i - 1]}.png` : undefined" @click="removeAwaken(i - 1);" :key="filter.awaken[i - 1] ? i : '0'">
-              </span>
-            </div>
-            <div>
-              <template v-for="(n, i) in awakenSortList">
-                <span v-if="n != 0" :key="`awakenList_${i}`">
-                  <img style="width: 24px; height: 24px; margin: 0 12px 12px 0; cursor: pointer;" :src="`./image/awaken/${n}.png`" @click="addAwaken(n);">
-                </span>
-                <br v-else :key="`awakenList_${i}`">
-              </template>
-            </div>
+            <awaken-select v-model="filter.awaken" />
           </div>
         </div>
         <button class="btn btn-secondary btn-sm" type="button" @click="clearFilter">クリア</button>
@@ -204,19 +192,6 @@ export default {
   computed: {
     attributeTable () { return constData.attributeTable; },
     typeTable () { return constData.typeTable; },
-    /** ゲーム内の覚醒フィルタでの覚醒の並び順を示すテーブル。 0 は改行。 */
-    awakenSortList () {
-      return [
-        4, 5, 6, 7, 8, 1, 49, 57, 0,
-        14, 15, 16, 17, 18, 2, 50, 58, 0,
-        22, 23, 24, 25, 26, 3, 51, 59, 0,
-        9, 10, 20, 11, 12, 13, 52, 60, 0,
-        19, 21, 27, 28, 29, 30, 53, 61, 0,
-        32, 31, 33, 34, 35, 36, 54, 62, 0,
-        37, 38, 39, 40, 41, 42, 55, 63, 0,
-        43, 44, 45, 46, 47, 48, 56, 64
-      ];
-    },
     /** スキルターンの絞り込み設定の最小値と最大値を - でつないだもの。 */
     skillTurnFilterStr: {
       get: function () {
@@ -370,15 +345,6 @@ export default {
           this.filter[key] = defaultValue;
         }
       }
-    },
-    /** 選択中の覚醒を追加する。 */
-    addAwaken: function (no) {
-      if (this.filter.awaken.length >= 9) { return; }
-      this.filter.awaken.push(no);
-    },
-    /** 選択中の覚醒から、指定したインデックスのものを削除する。 */
-    removeAwaken: function (index) {
-      this.filter.awaken.splice(index, 1);
     }
   }
 };
