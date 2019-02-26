9f15ef1b71653859a2b1c35c393b00bb47634b02
rainbowsazaki <rainbowsazaki@gmail.com>
2019-02-26 22:20:22 +0900

    [update] サブDBにコメント書き込みデータ記録用のテーブルを作成する SQL を追加。
    コメント元の url パスと一覧表示用にページタイトル、そして名前と本文とタイムスタンプなどなど。

--- db/table_sub.sql
+++ db/table_sub.sql
@@ -5,3 +5,14 @@ CREATE TABLE search_result (
   createdDatetime TIMESTAMP DEFAULT (datetime(CURRENT_TIMESTAMP,'localtime'))
 );
 
+CREATE TABLE bbs_entry (
+  pageUrl TEXT,
+  pageTitle TEXT,
+  name TEXT,
+  message TEXT,
+
+  timestamp TIMESTAMP DEFAULT (datetime(CURRENT_TIMESTAMP,'localtime')),
+  ipAddress TEXT,
+  accountName TEXT,
+  state INTEGER
+);
