588dabde21e29feaa76ba8068191f3cacd760d9a
rainbowsazaki <rainbowsazaki@gmail.com>
2019-03-15 21:09:40 +0900

    [add] モンスターの必要素材一覧表示ページのコンポーネント及びそこで使用するコンポーネントのファイルを追加。
    中身は、ページの方からコンポーネントを呼び出しているだけの、空に近い状態。

---
+++ client/src/views/pageEvolutionMaterial.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <evolution-material />
+  </div>
+</template>
+
+<script>
+import evolutionMaterial from '../components/evolutionMaterial.vue';
+
+export default {
+  components: {
+    'evolutionMaterial': evolutionMaterial
+  }
+};
+</script>
