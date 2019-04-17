96b8d9b0449b03d962b722ed1c231f27e964692c
rainbowsazaki <rainbowsazaki@gmail.com>
2019-04-17 20:48:14 +0900

    [update] モンスターフィルタリングコンポーネントの、モンスター配列をフィルタリングする関数で、フィルタリングの無い設定の場合はフィルタリング処理を行わないよう変更。
    モンスター一覧やランキングページで使用しているもの。
    先の変更 (72d8692b785f48f411efb36339e9c3c6938a6b2a) で追加した isAll プロパティを見てフィルタリング処理を行うか判断し、不要ならもととなる配列をそのまま返す。

--- client/src/components/monsterFilterSetting.vue
+++ client/src/components/monsterFilterSetting.vue
@@ -204,6 +204,7 @@ export function getFilterFunction (setting) {
 /** フィルタリング設定を使用してモンスター情報の配列から、フィルタリング情報の条件を満たすもののみ取り出した配列を作成する。 */
 export function filterMonsterDataArray (setting, target) {
   const func = getFilterFunction(setting);
+  if (func.isAll) { return target; }
   return target.filter(d => func(d.data || d));
 }
 
