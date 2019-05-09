3b577e804c8dad826d111d84b96a8d7ddccacf79
rainbowsazaki <rainbowsazaki@gmail.com>
2019-05-09 20:38:57 +0900

    [update] モンスター情報表示ページの情報表示部用に、ゲーム中で使われているフォントに若干近いフォントのWebフォントを読み込み、使用する指定を追加。
    太めのゴシック系？

--- client/src/views/pageMonsterData.vue
+++ client/src/views/pageMonsterData.vue
@@ -7,7 +7,7 @@
     <div v-if="!isHistory"><tweet-button v-if="monsterData.no" /></div>
     <div class="row">
       <div class="col-md-6">
-        <div id="monsterInfo" style="color: #FFF; line-height: 1.3em; text-shadow: 0.1em 0.1em 0 rgba(0,0,0, 0.5); background: #000; border: 1px solid black; margin-bottom: 1rem;" :style="{ 'font-size': `${infoFontSize}px` }">
+        <div id="monsterInfo" style="color: #FFF; line-height: 1.3em; font-family: 'M PLUS 1p', sans-serif; text-shadow: 0.1em 0.1em 0 rgba(0,0,0, 0.5); background: #000; border: 1px solid black; margin-bottom: 1rem;" :style="{ 'font-size': `${infoFontSize}px` }">
           <div style="background: linear-gradient(#798320, #394a14); border: 0.1em #b1ba39 solid; border-left: none; margin-top: 1em; margin-right: 5em; padding: 0.1em 0em 0.2em 4.4em; line-height: 1.1em; border-radius: 0 0.5em 0.5em 0;">
             <span style="display: inline-block; width: 6.5em;">No.{{ monsterData.no }}</span><span style="color: #EE0"><template v-for="n in monsterData.rare">★</template></span><br>
             {{ monsterData.name }}
@@ -517,6 +517,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+@import url('https://fonts.googleapis.com/css?family=M+PLUS+1p:700,900');
 
 .paramAlert {
   color: rgba(224, 0, 0, 0.8);
