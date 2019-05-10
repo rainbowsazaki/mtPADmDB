5b3879c9c1430d3e463fa1dda21ee47a94cd5b99
rainbowsazaki <rainbowsazaki@gmail.com>
2019-05-10 23:12:52 +0900

    [update] モンスター情報表示ページで、長いモンスター名を表示する際に横幅が収まるように縮小する機能を追加。
    要素構成を整理して、スキルの効果説明分の処理と同様の処理を適用させた。

--- client/src/views/pageMonsterData.vue
+++ client/src/views/pageMonsterData.vue
@@ -9,8 +9,13 @@
       <div class="col-md-6">
         <div id="monsterInfo" style="color: #FFF; line-height: 1.3em; font-family: 'M PLUS 1p', sans-serif; text-shadow: 0.1em 0.1em 0 rgba(0,0,0, 0.5); background: #000; border: 1px solid black; margin-bottom: 1rem;" :style="{ 'font-size': `${infoFontSize}px` }">
           <div style="background: linear-gradient(#798320, #394a14); border: 0.1em #b1ba39 solid; border-left: none; margin-top: 1em; margin-right: 5em; padding: 0.1em 0em 0.2em 4.4em; line-height: 1.1em; border-radius: 0 0.5em 0.5em 0;">
-            <span style="display: inline-block; width: 6.5em;">No.{{ monsterData.no }}</span><span style="color: #EE0; -webkit-text-stroke: 0.05em #660;"><template v-for="n in monsterData.rare">★</template></span><br>
-            {{ monsterData.name }}
+            <div>
+              <span style="display: inline-block; width: 6.5em;">No.{{ monsterData.no }}</span>
+              <span style="color: #EE0; -webkit-text-stroke: 0.05em #660;"><template v-for="n in monsterData.rare">★</template></span>
+            </div>
+            <div>
+              <div class="stretch" style="white-space: nowrap;">{{ monsterData.name }}</div>
+            </div>
           </div>
           <div style="position: relative; height: 17em;">
             <div class="monsterImage" style="height: 100%;">
