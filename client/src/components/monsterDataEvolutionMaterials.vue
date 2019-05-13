ee294d8f5ea26eb192d3f67e385d82348ed74558
rainbowsazaki <rainbowsazaki@gmail.com>
2019-05-13 21:16:37 +0900

    [update] モンスター進化情報コンポーネントの進化情報部分の内側縁に光沢を意図した白い影を追加。
    ゲーム画面だと縁が光っている感じなので、それを近い感じに再現。

--- client/src/components/monsterDataEvolutionMaterials.vue
+++ client/src/components/monsterDataEvolutionMaterials.vue
@@ -90,6 +90,12 @@ export default {
     border-right: none;
     padding: 0.1em;
     border-radius: 6px 0 0 6px;
+    
+    box-shadow:
+      0 4px 2px -2px rgba(255,255,0,0.5) inset,
+      3px 0 2px -2px rgba(255,255,0,0.5) inset,
+      0 -2px 2px -2px rgba(255,255,0,0.5) inset
+    ;
   }
 
   /* これを .baseIcon の中に入れると下の .type? .typeName より優先度が高くなり下でのクラデーション背景指定が効かなくなるの外に出しておく。 */
@@ -119,6 +125,11 @@ export default {
     padding-top: 0.5em;
     padding-left: 0.3em;
     border-radius: 0 6px 6px 0;
+    
+    box-shadow:
+      0 4px 2px -2px rgba(255,255,0,0.5) inset,
+      -3px 0 2px -2px rgba(255,255,0,0.5) inset,
+      0 -2px 2px -2px rgba(255,255,0,0.5) inset;
 
     .monsterName {
       border: 0.1em solid #5b401e;
