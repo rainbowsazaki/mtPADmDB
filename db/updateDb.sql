fdfb4181144f71c0c95a4db79232051d0d0037d4
rainbowsazaki <rainbowsazaki@gmail.com>
2019-06-11 14:40:05 +0900

    [update] データベースのスキルターン関連構造更新SQLの maxLevel 項目を削除する部分を有効化。
    サーバー処理で maxLevel を使わなくなったので削除が可能になった。

--- db/updateDb.sql
+++ db/updateDb.sql
@@ -6,27 +6,27 @@ UPDATE skill
   WHERE baseTurn IS NOT NULL AND maxLevel IS NOT NULL;
 
 
--- -- maxLevel を削除したいが、 SQLite にはテーブルの項目の削除がないため、
--- -- 削除したい項目がないテーブルを別途作成して、そこに内容をコピーして元のテーブルを削除する。
--- CREATE TABLE IF NOT EXISTS skill_copy (
---   id INTEGER PRIMARY KEY,
---   no INTEGER,
---   name TEXT,
---   description TEXT,
---   baseTurn INTEGER,
---   minTurn INTEGER,
---   comment TEXT,
+-- maxLevel を削除したいが、 SQLite にはテーブルの項目の削除がないため、
+-- 削除したい項目がないテーブルを別途作成して、そこに内容をコピーして元のテーブルを削除する。
+CREATE TABLE IF NOT EXISTS skill_copy (
+  id INTEGER PRIMARY KEY,
+  no INTEGER,
+  name TEXT,
+  description TEXT,
+  baseTurn INTEGER,
+  minTurn INTEGER,
+  comment TEXT,
 
--- 	createdDatetime TIMESTAMP DEFAULT (datetime(CURRENT_TIMESTAMP,'localtime')),
---   ipAddress TEXT,
---   accountName TEXT,
---   state INTEGER
--- );
+	createdDatetime TIMESTAMP DEFAULT (datetime(CURRENT_TIMESTAMP,'localtime')),
+  ipAddress TEXT,
+  accountName TEXT,
+  state INTEGER
+);
 
--- INSERT INTO skill_copy
--- 	SELECT id, no, name, description, baseTurn, minTurn, comment, createdDatetime, ipAddress, accountName, state
--- 	FROM  skill;
+INSERT INTO skill_copy
+	SELECT id, no, name, description, baseTurn, minTurn, comment, createdDatetime, ipAddress, accountName, state
+	FROM  skill;
 	
--- DROP TABLE skill;
--- ALTER TABLE skill_copy RENAME TO skill;
--- VACUUM;
+DROP TABLE skill;
+ALTER TABLE skill_copy RENAME TO skill;
+VACUUM;
