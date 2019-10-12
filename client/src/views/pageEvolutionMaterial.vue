6b11fca281f3b1d1b1c29279f1bfb24f379c0079
rainbowsazaki <rainbowsazaki@gmail.com>
2019-10-12 21:57:45 +0900

    [update] モンスター必要素材一覧ページに進化系統や素材をツリー形式で格納したオブジェクトを作成する処理を追加。
    これまでは表示用のコンポーネントで再帰させて進化系統や素材をツリー状に表示していたが、それだと進化後が上に来る形でないとうまく表示できない。一方ゲーム内での進化系統の表示では進化前が上に来ていて、それに表示を合わせるためにツリー構造のデータを作成してからそれをコンポーネントに渡して表示するという形変更することに。
    その最初の段階として、ツリー構造のデータを作成。

--- client/src/views/pageEvolutionMaterial.vue
+++ client/src/views/pageEvolutionMaterial.vue
@@ -55,6 +55,16 @@ export default {
   },
   data: function () {
     return {
+    /** 表示するモンスターの進化情報をツリー形式で格納したオブジェクト。
+     * {
+     *   no: 表示するモンスターの番号。
+     *   kiseki: 希石化して使用する場合の希石の番号。ベースのモンスターに入れておく。
+     *   materials: このモンスターを進化させる場合に必要な素材の進化情報配列。
+     *              中身はevoInfoと同じ構造のオブジェクト。
+     *   evo: 進化後のモンスターの進化情報。中身はevoInfoと同じ構造のオブジェクト。
+     * }
+     */
+      evoInfo: {},
       /**
        * 対象モンスターを作成するのに必要な進化元・素材の数の配列。
        * [ { no: モンスター番号, count: 必要個数 }, ... ]
@@ -74,6 +84,12 @@ export default {
       return `No.${this.no || this.monsterData.no} ${this.monsterData.name} の作成に必要な全モンスター一覧`;
     }
   },
+  watch: {
+    no: 'createEvoInfo'
+  },
+  created: function () {
+    this.createEvoInfo();
+  },
   methods: {
     /** 必要素材数を受け取るメソッド。 */
     onTotalMaterialCounts (obj) {
@@ -81,6 +97,67 @@ export default {
       for (const no in obj) {
         array.push({ no: no, count: obj[no] });
       }
+      // 指定された番号のモンスターが進化のための素材となるモンスターかどうかを取得する。
+      const isMaterial = (no) => {
+        const monsterData = this.monsterTable[no];
+        if (!monsterData) { return false; }
+        const type = monsterData.types[0];
+        // 進化系と強化系を素材系タイプとみなす。
+        return type === 9 || type === 11;
+      };
+      array.sort((a, b) => isMaterial(a.no) - isMaterial(b.no));
+      this.sortedMaterialList = array;
+    },
+    /** 表示するモンスターの進化情報をツリー形式で格納したオブジェクトを作成する。 */
+    createEvoInfo () {
+      const needMaterialCounts = {};
+      function AddNeedMaterials (no) {
+        if (needMaterialCounts[no]) {
+          needMaterialCounts[no]++;
+        } else {
+          needMaterialCounts[no] = 1;
+        }
+      }
+      const monsterTable = this.monsterTable;
+      const monsterArray = Object.values(monsterTable);
+
+      function func (monsterNo, evoObj, evoMaterials, kiseki) {
+        let monsterData = monsterTable[monsterNo];
+        const obj = { no: monsterNo };
+        if (/^(.+)の希石$/.test(monsterData.name)) {
+          const baseName = RegExp.$1;
+          const baseData = monsterArray.find(d => d.name === baseName);
+          if (baseData) {
+            monsterData = baseData;
+            kiseki = monsterNo;
+            obj.no = baseData.no;
+          }
+        }
+        if (monsterData.types[0] > 8) {
+          AddNeedMaterials(obj.no);
+          if (kiseki) {
+            obj.kiseki = kiseki;
+            return obj;
+          }
+          return null;
+        }
+
+        if (evoObj) { obj.evo = evoObj; }
+        if (evoMaterials && evoMaterials.length) { obj.materials = evoMaterials; }
+        if (!monsterData.evolution.baseNo) {
+          AddNeedMaterials(obj.no);
+          if (kiseki) { obj.kiseki = kiseki; }
+          return obj;
+        }
+
+        const needMaterials = monsterData.evolution.materials.map(d => func(d)).filter(d => d);
+        return func(monsterData.evolution.baseNo, obj, needMaterials, kiseki);
+      }
+      this.evoInfo = func(this.no);
+
+      const array = Object.keys(needMaterialCounts).map(
+        no => ({ no: no, count: needMaterialCounts[no] })
+      );
       // 指定された番号のモンスターが進化のための素材となるモンスターかどうかを取得する。
       const isMaterial = (no) => {
         const monsterData = this.monsterTable[no];
