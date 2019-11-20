fa78cf85345cfff16c2d59130a723cd5d569fe8e
rainbowsazaki <rainbowsazaki@gmail.com>
2019-11-20 21:54:56 +0900

    [update] $route.query ラッパーミックスインに、読み込み設定情報を取得する処理を追加。
    ミックスイン先で定義された、 routeQueryWrapper 内の値を読み込み、自身で用意したオブジェクトにコピーする。

--- client/src/components/mixins/routeQueryWrapper.js
+++ client/src/components/mixins/routeQueryWrapper.js
@@ -2,11 +2,21 @@
 export default {
   data: function () {
     return {
+      /** 読み込み対象の情報を格納するオブジェクト。 */
+      '$routeQueryWrapper_info': {}
     };
   },
   watch: {
   },
   created: function () {
+    const rqw = this.routeQueryWrapper;
+    if (!rqw) { return; }
+    const info = this.$data.$routeQueryWrapper_info;
+    for (const key in rqw) {
+      let value = rqw[key];
+      if (!value.type) { value = { type: value }; }
+      info[key] = value;
+    }
   },
   methods: {
   }
