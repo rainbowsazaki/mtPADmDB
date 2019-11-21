43f63f0bf1dfde859a8f5cf70228a8e5a2ac0ae6
rainbowsazaki <rainbowsazaki@gmail.com>
2019-11-21 21:20:24 +0900

    [fix] $route.query ラッパーミックスインの初期化処理で作成した読み込み設定情報が、キーの列挙ができないのを修正。
    次の処理を作成する上で問題となったので先に修正。
    原因はよくわからないが、初期値の空オブジェクトをそのまま使用したらダメだった。
    初期化処理内で作成した空オブジェクトを元にしたら問題なかった。

--- client/src/components/mixins/routeQueryWrapper.js
+++ client/src/components/mixins/routeQueryWrapper.js
@@ -11,12 +11,13 @@ export default {
   created: function () {
     const rqw = this.routeQueryWrapper;
     if (!rqw) { return; }
-    const info = this.$data.$routeQueryWrapper_info;
+    const info = {};
     for (const key in rqw) {
       let value = rqw[key];
       if (!value.type) { value = { type: value }; }
       info[key] = value;
     }
+    this.$data.$routeQueryWrapper_info = info;
   },
   methods: {
   }
