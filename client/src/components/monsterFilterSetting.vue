a4163378d548fae0bb2d0a9d531089857487917f
rainbowsazaki <rainbowsazaki@gmail.com>
2019-12-31 17:49:29 +0900

    [fixed] モンスターフィルタリングコンポーネントのフォームで設定を変更したときに反映されないことがあるのを修正。
    前回の変更時にうまく動いていたのは、queriesReceived に指定した更新通知で渡したオブジェクトの中身を変更したことによって、変更通知なしに変更内容を取得できていたからのようで、クエリ指定なしでページを開いた状態でフォームの内容を変更しても反映されなかった。
    queriesReceived と queriesSended の両方で更新通知を送るようにして対応。

--- client/src/components/monsterFilterSetting.vue
+++ client/src/components/monsterFilterSetting.vue
@@ -369,6 +369,10 @@ export default {
       computed: true
     }
   },
+  /** データの変更を受けて $route.query が変更されたときに呼ばれるフック。 */
+  queriesSended: function () {
+    this.$emit('input', this.filter);
+  },
   /** $route.query の変更を受けてデータが変更されたときに呼ばれるフック。 */
   queriesReceived: function () {
     this.$emit('input', this.filter);
