3a6e34ea90fbccc18bbfe932b8341c5e12d1ecac
rainbowsazaki <rainbowsazaki@gmail.com>
2020-03-07 20:32:56 +0900

    [update] モンスターランキングページのモンスターアイコンに、お気に入りに入っていることを示すマークを表示するよう属性指定を変更。
    合わせて、現状では指定不要なモンスターテーブル情報とアイコン情報の指定を削除。

--- client/src/views/pageRanking.vue
+++ client/src/views/pageRanking.vue
@@ -110,7 +110,7 @@
         </div>
         <div class="cell">
           <span class="monsterIconWrapper">
-            <monster-icon style="line-height: 1em;" :no="data.data.no" :monster-table="monsterTable" :image-table="imageTable" width="3em" height="3em" />
+            <monster-icon style="line-height: 1em;" use-favorite-flag :no="data.data.no" width="3em" height="3em" />
             {{ (_senzaiKillerNo = data.data.enableSenzaiKiller) && null }}
             <img v-if="_senzaiKillerNo" class="senzaiIcon" :src="`image/senzaiKiller/${_senzaiKillerNo}.png`">
             {{ (_superAwakenNo = data.data.bestSuperAwaken) && null }}
