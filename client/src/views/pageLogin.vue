9518ca0fc9dbc6e8c49eec92592c0c17fa2ccc3d
rainbowsazaki <rainbowsazaki@gmail.com>
2020-03-16 22:11:03 +0900

    [update] ログインページで、ログイン・ログアウト処理が完了したときは、それが完了したことを示す表示を行うよう変更。
    これまでは、ログイン・ログアウトが完了したときにはログアウト・ログインを行う表示に切り替えていたが、処理が正しく完了したことが分かりづらいので、完了したことを表示するように変更。
    これに伴い、ページを表示した時点でのログイン状態を保持しておく必要でたので、それを保持する変数及び取得を行う処理を追加。

--- client/src/views/pageLogin.vue
+++ client/src/views/pageLogin.vue
@@ -1,22 +1,28 @@
 <template>
-  <div v-if="!accountData">
-    ログイン確認中...
-  </div>
-  <div v-else-if="accountData.uid">
-    <h2>ログアウト</h2>
-    <div class="loginButton">
-      <button class="btn btn-primary" @click="signOut">ログアウトする</button>
-    </div>
-  </div>
-  <div v-else>
-    <h2>ログイン</h2>
-    <div class="loginButton">
-      <button class="btn btn-primary" @click="signIn">Twitter アカウントでログイン</button>
-    </div>
-    <h3 class="decoHeader">ログインすることでできること</h3>
-    <h4>お気に入りモンスターをサーバーに記録</h4>
-    <p>ログインしていない場合はお気に入りモンスターはブラウザごとのストレージに記録しているため、別のブラウザや端末間で共有や引き継ぎをすることができません。</p>
-    <p>ログインすることでアカウントごとにサーバーに記録するようになり、同じアカウントでログインすることで別ブラウザ・別端末からも同じデータを使用することができるようになります。</p>
+  <div>
+    <template v-if="isLoginStart === undefined">
+      ログイン確認中...
+    </template>
+    <template v-else-if="isLoginStart">
+      <h2>ログアウト</h2>
+      <div v-if="isLogined" class="loginButton">
+        <button class="btn btn-primary" @click="signOut">ログアウトする</button>
+      </div>
+      <div v-else class="loginButton">ログアウトしました。</div>
+    </template>
+    <template v-else>
+      <h2>ログイン</h2>
+      <div v-if="!isLogined" class="loginButton">
+        <button class="btn btn-primary" @click="signIn">Twitter アカウントでログイン</button>
+      </div>
+      <div v-else class="loginButton">
+        ログインしました。
+      </div>
+      <h3 class="decoHeader">ログインすることでできること</h3>
+      <h4>お気に入りモンスターをサーバーに記録</h4>
+      <p>ログインしていない場合はお気に入りモンスターはブラウザごとのストレージに記録しているため、別のブラウザや端末間で共有や引き継ぎをすることができません。</p>
+      <p>ログインすることでアカウントごとにサーバーに記録するようになり、同じアカウントでログインすることで別ブラウザ・別端末からも同じデータを使用することができるようになります。</p>
+    </template>
   </div>
 </template>
 
@@ -31,9 +37,15 @@ import MixinForPage from '../components/mixins/forPage.js';
 export default {
   name: 'PageLogin',
   pageTitle: function () {
-    return (this.isLogined) ? 'ログアウト' : 'ログイン';
+    return (this.isLoginStart) ? 'ログアウト' : 'ログイン';
   },
   mixins: [MixinForPage],
+  data: function () {
+    return {
+      /** ページにアクセスした時点でログインしていたかどうか。 */
+      isLoginStart: undefined
+    };
+  },
   computed: {
     /** ログインしているアカウントの情報。 */
     accountData: function () {
@@ -41,13 +53,31 @@ export default {
     },
     /** ログインしているかどうか。 */
     isLogined: function () {
-      return !!this.accountData.uid;
+      const accountData = this.accountData;
+      return accountData && !!accountData.uid;
     }
   },
   watch: {
-    'isLogined': '$_mixinForPage_updateTitle'
+    'isLoginStart': '$_mixinForPage_updateTitle',
+    'accountData': function () {
+      // コンポーネント作成時点でログイン確認が終わっていなかった場合用の処理。
+      // ログイン確認が終わって accountData にオブジェクトが入った直後には aid などに値が入っていないようなので、少し間をおいて取得する。
+      if (this.isLoginStart === undefined) {
+        setTimeout(() => this.checkLoginStart(), 10);
+      }
+    }
+  },
+  created: function () {
+    this.checkLoginStart();
   },
   methods: {
+    /** ページ開始時にログインしているかどうかを確認する。 */
+    checkLoginStart () {
+      // 確認済みの場合は処理を行わない。
+      if (this.isLoginStart === undefined && this.accountData) {
+        this.isLoginStart = this.isLogined;
+      }
+    },
     /** ログイン処理。 */
     signIn: function () {
       signIn();
