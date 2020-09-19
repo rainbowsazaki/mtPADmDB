45ec0e34ced919bd9f627147477e88f81fd49ea8
rainbowsazaki <rainbowsazaki@gmail.com>
2020-09-19 17:52:30 +0900

    [fix] DBに潜在覚醒枠解放可不可の情報を追加する SQL で、進化形態が不明 (NULL) の場合に枠解放可不可も NULL になってしまうのを修正。
    真偽値をそのまま入れていたのが良くなかった様子。
    WHERE を使用して対象条件のものに 1 を入れる形に変更。

--- db/updateDb.sql
+++ db/updateDb.sql
@@ -1,2 +1,2 @@
 ALTER TABLE monster_base_data ADD canUnlockExtraSlot INTEGER DEFAULT 0;
-UPDATE monster_base_data SET canUnlockExtraSlot = evolutionType IN (3, 6, 7) OR name LIKE '%耳飾り';
+UPDATE monster_base_data SET canUnlockExtraSlot = 1 WHERE evolutionType IN (3, 6, 7) OR name LIKE '%耳飾り';
